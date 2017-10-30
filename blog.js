2b86fb93ccb9c6d84dce4e0cc1687ca07bbf55b5
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-10-30 09:23:45 +0100

    functional first version

---
+++ blog.js
@@ -0,0 +1,16 @@
+const entries = require('./data/entries.json')
+
+function getBlogEntries() {
+  return entries;
+}
+
+function getBlogEntry(id) {
+  return entries.find( entry => entry.id === id)
+}
+
+function addBlogEntry(post) {
+  entries.push(post)
+}
+
+
+module.exports = { getBlogEntries, getBlogEntry, addBlogEntry }
